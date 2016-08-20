83d5c1bbc2b5ecce7b88589355aa47a2c0941220
gitSonar <pcadmin@INFPU011152.puneodc.lntinfotech.com>
2016-08-20 09:55:49 +0530

    Added
    Updated HelloMainCls.java file

--- HelloWorld/src/HelloPkj/HelloMainCls.java
+++ HelloWorld/src/HelloPkj/HelloMainCls.java
@@ -7,7 +7,7 @@ public class HelloMainCls {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("This is my first Github Hello World");
 
 	}
 
